050fd1708c6dcafca32b0bf3f8674c5378cda717
swyoo <getampain@gmail.com>
2020-05-14 18:09:56 +0900

    add 2020-05-14 mysql 연동

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask
 from flask_restful import Resource, Api
+import pymysql
+
+
 import json
 
 app = Flask(__name__)
@@ -16,10 +19,28 @@ class TestPost(Resource):
 
 class TestGet(Resource):
     def get(self, id, pw):
-        if id == "abc" and pw == "1234":
-            return {'get_test': 'success'}
+        conn = pymysql.connect(host='34.84.246.239', port=3306, user='root', passwd='1234', db='flask')
+        cur = conn.cursor()
+        sql = "SELECT * FROM flask.userInfo WHERE userid = '"+id+"' and userpw = '"+pw+"' "
+        row = cur.execute(sql)
+        result = cur.fetchall()
+
+        #conn.commit()
+        print("!!!!"+sql)
+        print(row)
+
+        #return row
+
+        if(row == 0 ):
+            return {'get_test': "not exist"}
         else:
-            return {'get_test': 'fail'}, 404
+            return {'get_test': result[0]}
+
+
+        """if id == "abc" and pw == "1234":
+            return {'get_test': row[0]}
+        else:
+            return {'get_test': 'fail'}, 404 """
 
 
 api.add_resource(TestPost, '/', '/testp/<int:id>')
